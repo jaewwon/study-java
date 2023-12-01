43e30df0218fd1ea4566d7be27ad25ff0919569a
jaewwon <j04769842@gmail.com>
2023-12-01 19:54:14 +0900

    study 231201

---
+++ chap02/chap02_ex02/src/CircleArea.java
@@ -0,0 +1,16 @@
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		final double PI = 3.14;
+		double radius = 10.0;
+		double circleArea = radius*radius*PI;
+		
+		System.out.println("Area of Circle = "+ circleArea);
+		//Area of Circle = 314.0
+
+	}
+
+}
